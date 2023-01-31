cf53de824f58a1f473c81d2d860c226bc66b1f48
Damien BELGHIT <damien.belghit@yahoo.fr>
2023-01-31 15:11:26 +0100

    Initialisation

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Profile from './pages/Profile.jsx'
+import './styles/base.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Profile />
+  </React.StrictMode>,
+)
---
+++ src/pages/Profile.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import '../styles/Profile.css'
+
+function Profile() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div >
+      Hey
+    </div>
+  )
+}
+
+export default Profile
